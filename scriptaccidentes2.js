ac38adbf774d104b9706f43422e9623a3b40d6e1
rstuardoc <rstuardoc@uc.cl>
2021-05-31 20:23:44 -0400

    ultimo subido

--- scriptaccidentes2.js
+++ scriptaccidentes2.js
@@ -4,26 +4,59 @@ $(document).ready(function(){
     $("#g3").hide();
     $("#g4").hide();
     $("#g5").hide();
-    $("#causa1").click(function(){
-        $("#g1").hide();
-        $("#g2").show();
+    $("#tg1").hide();
+    $("#tg2").hide();
+    $("#tg3").hide();
+    $("#tg4").hide();
+    $("#tg5").hide();
+
+    $("#causa").click(function(){
+        $("#g1").show();
+        $("#tg1").show();
+        $("#g2").hide();
         $("#g3").hide();
         $("#g4").hide();
         $("#g5").hide();
+        $("#tg2").hide();
+        $("#tg3").hide();
+        $("#tg4").hide();
+        $("#tg5").hide();
+    });
+    $("#causa1").click(function(){
+        $("#g1").hide();
+        $("#g2").show();
+        $("#tg2").show();
+        $("#g3").hide();
+        $("#g4").hide();
+        $("#g5").hide();
+        $("#tg1").hide();
+        $("#tg3").hide();
+        $("#tg4").hide();
+        $("#tg5").hide();
     });
     $("#causa2").click(function(){
         $("#g1").hide();
         $("#g2").hide();
         $("#g3").show();
+        $("#tg3").show();
         $("#g4").hide();
         $("#g5").hide();
+        $("#tg2").hide();
+        $("#tg1").hide();
+        $("#tg4").hide();
+        $("#tg5").hide();
     });
     $("#causa3").click(function(){
         $("#g1").hide();
         $("#g2").hide();
         $("#g3").hide();
         $("#g4").show();
+        $("#tg4").show();
         $("#g5").hide();
+        $("#tg2").hide();
+        $("#tg3").hide();
+        $("#tg1").hide();
+        $("#tg5").hide();
     });
     $("#causa4").click(function(){
         $("#g1").hide();
@@ -31,6 +64,11 @@ $(document).ready(function(){
         $("#g3").hide();
         $("#g4").hide();
         $("#g5").show();
+        $("#tg5").show();
+        $("#tg2").hide();
+        $("#tg3").hide();
+        $("#tg4").hide();
+        $("#tg1").hide();
     });
 
 
@@ -69,5 +107,53 @@ $(document).ready(function(){
     $("#historia").mouseleave(function(){
         $(this).fadeTo("fast",1);
                     
+    });   
+    
+    $("#causa").mouseenter(function(){
+        $(this).fadeTo("fast",0.5);
+                    
+    });
+                    
+    $("#causa").mouseleave(function(){
+        $(this).fadeTo("fast",1);
+                    
     });      
+    $("#causa1").mouseenter(function(){
+        $(this).fadeTo("fast",0.5);
+                    
+    });
+                    
+    $("#causa1").mouseleave(function(){
+        $(this).fadeTo("fast",1);
+                    
+    });  
+    $("#causa2").mouseenter(function(){
+        $(this).fadeTo("fast",0.5);
+                    
+    });
+                    
+    $("#causa2").mouseleave(function(){
+        $(this).fadeTo("fast",1);
+                    
+    });    
+    $("#causa3").mouseenter(function(){
+        $(this).fadeTo("fast",0.5);
+                    
+    });
+                    
+    $("#causa3").mouseleave(function(){
+        $(this).fadeTo("fast",1);
+                    
+    }); 
+    $("#causa4").mouseenter(function(){
+        $(this).fadeTo("fast",0.5);
+                    
+    });
+                    
+    $("#causa4").mouseleave(function(){
+        $(this).fadeTo("fast",1);
+                    
+    }); 
+
+
 });      
--- tecnologias.js
+++ tecnologias.js
@@ -5,29 +5,15 @@ $(document).ready(function(){
     $(".txcinat").hide();
     $(".txsilla").hide();
     $(".txairbag").hide();
-    $("#cat1").hide();
-    $("#cat2").hide();
-    $("#cad1").hide();
-    $("#cad2").hide();
-    $("#s1").hide();
-    $("#s2").hide();
-    $("#a1").hide();
-    $("#a2").hide();
-    $(".semicirculo").hide();
-    $(".triangulo").hide();
-    $("#a2").hide();
-    $(".vel1").hide();
-    $(".vel2").hide();
-    $(".vel3").hide();
-    $(".vel4").hide();
     var ccinad=true;
     var ccinat=true;
     var csilla=true;
     var cairbag=true;
-    var data1 =[89.97,80];
-    var data2 =[89.3,85.8];
-    var data3 =[30,-12];
-    var data4 =[79.6,71.8];
+    $("#gr1").hide();
+    $("#gr2").hide();
+    $("#gr3").hide();
+    $("#gr4").hide();
+
 
     $("#cinad").mouseenter(function(){
         $(this).fadeTo("fast",1);
@@ -54,31 +40,10 @@ $(document).ready(function(){
         $(".txcinat").hide();
         $(".txsilla").hide();
         $(".txairbag").hide();
-        $("#cat1").hide();
-        $("#cat2").hide();
-        $("#cad1").show();
-        $("#cad2").show();
-        $("#s1").hide();
-        $("#s2").hide();
-        $("#a1").hide();
-        $("#a2").hide();
-        $(".semicirculo").show();
-        $(".triangulo").show();   
-        $(".vel1").show();
-        $(".vel2").hide();
-        $(".vel3").hide();
-        $(".vel4").hide();
-        $("#cad1").click(function(){
-            $(".triangulo").css({"transform":"rotate(89.97deg)"});
-            $(this).css({"background-color":"red"});
-            $("#cad2").css({"background-color":"transparent"})
-        });
-        $("#cad2").click(function(){
-            $(".triangulo").css({"transform":"rotate(80deg)"});
-            $(this).css({"background-color":"red"});
-            $("#cad1").css({"background-color":"transparent"});
-            
-        });
+        $("#gr1").show();
+        $("#gr2").hide();
+        $("#gr3").hide();
+        $("#gr4").hide();
          
     });
 
@@ -106,30 +71,10 @@ $(document).ready(function(){
         $(".txcinat").show();
         $(".txsilla").hide();
         $(".txairbag").hide();
-        $("#cat1").show();
-        $("#cat2").show();
-        $("#cad1").hide();
-        $("#cad2").hide();
-        $("#s1").hide();
-        $("#s2").hide();
-        $("#a1").hide();
-        $("#a2").hide();  
-        $(".semicirculo").show();
-        $(".triangulo").show();  
-        $(".vel1").hide(); 
-        $(".vel2").show();
-        $(".vel3").hide();
-        $(".vel4").hide();
-        $("#cat1").click(function(){
-            $(".triangulo").css({"transform":"rotate(89.3deg)"});
-            $(this).css({"background-color":"red"});
-            $("#cat2").css({"background-color":"transparent"})
-        });
-        $("#cat2").click(function(){
-            $(".triangulo").css({"transform":"rotate(85.8deg)"});
-            $(this).css({"background-color":"red"});
-            $("#cat1").css({"background-color":"transparent"})
-        });
+        $("#gr1").hide();
+        $("#gr2").show();
+        $("#gr3").hide();
+        $("#gr4").hide();
     });
 
     $("#silla").mouseenter(function(){
@@ -156,30 +101,6 @@ $(document).ready(function(){
         $(".txcinat").hide();
         $(".txsilla").show();
         $(".txairbag").hide();
-        $("#cat1").hide();
-        $("#cat2").hide();
-        $("#cad1").hide();
-        $("#cad2").hide();
-        $("#s1").show();
-        $("#s2").show();
-        $("#a1").hide();
-        $("#a2").hide(); 
-        $(".semicirculo").show();
-        $(".triangulo").show(); 
-        $(".vel1").hide();  
-        $(".vel2").hide();
-        $(".vel3").show();
-        $(".vel4").hide();
-        $("#s1").click(function(){
-            $(".triangulo").css({"transform":"rotate(30deg)"});
-            $(this).css({"background-color":"red"});
-            $("#s2").css({"background-color":"transparent"})
-        });
-        $("#s2").click(function(){
-            $(".triangulo").css({"transform":"rotate(-12deg)"});
-            $(this).css({"background-color":"red"});
-            $("#s1").css({"background-color":"transparent"})
-        });
     });
 
     $("#airbag").mouseenter(function(){
@@ -206,30 +127,10 @@ $(document).ready(function(){
         $(".txcinat").hide();
         $(".txsilla").hide();
         $(".txairbag").show();
-        $("#cat1").hide();
-        $("#cat2").hide();
-        $("#cad1").hide();
-        $("#cad2").hide();
-        $("#s1").hide();
-        $("#s2").hide();
-        $("#a1").show();
-        $("#a2").show();
-        $(".semicirculo").show();
-        $(".triangulo").show(); 
-        $(".vel1").hide();  
-        $(".vel2").hide();
-        $(".vel3").hide();
-        $(".vel4").show();
-        $("#a1").click(function(){
-            $(".triangulo").css({"transform":"rotate(79.6deg)"});
-            $(this).css({"background-color":"red"});
-            $("#a2").css({"background-color":"transparent"})
-        });
-        $("#a2").click(function(){
-            $(".triangulo").css({"transform":"rotate(71.8deg)"});
-            $(this).css({"background-color":"red"});
-            $("#a1").css({"background-color":"transparent"})
-        });
+        $("#gr1").hide();
+        $("#gr2").hide();
+        $("#gr3").hide();
+        $("#gr4").show();
     });
 
     $("#historia").mouseenter(function(){
